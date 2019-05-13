106858b833f1cde6bbccdb4fd3ecb2e8df9c015b
Kuroki <kurokiroku1989@gmail.com>
2019-05-13 14:00:01 +0900

    【React.js#3】ルーティングを試してみた話

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 import Button from "./Button";
 
-// import ThreeSampleMoji from "./components/ThreeSampleMoji.js";
+// ルーティングの処理を追記(#3)
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import ThreeSampleMoji from "./components/ThreeSampleMoji.js";
 import ThreeSampleCube from "./components/ThreeSampleCube.js";
 import './ThreeSample.css';
 
@@ -12,17 +15,13 @@ export default class App extends Component {
       <div className="App">
         <header className="App-header">
           <h3>React.jsはじめました。</h3>
-          { /* コメントアウト
-          <div className="ThreeSampleMoji">
-            <ThreeSampleMoji />
-          </div>
-          */ } 
-          <div>
-            <ThreeSampleCube />
-          </div>
-          <Button name="menu1" />
-          <Button name="menu2" />
-          <Button name="menu3" />
+          <Router>
+            <Route className="ThreeSampleMoji" path='/ThreeSampleMoji' component={ThreeSampleMoji}/>
+            <Route path='/ThreeSampleCube' component={ThreeSampleCube}/>
+            <Button linkName="ThreeSampleCube" name="3Dキューブを表示" />
+            <Button linkName="ThreeSampleMoji" name="3Dタイトルを表示" />
+            <Button linkName="" name="何も表示しない" />
+          </Router>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
--- src/Button.js
+++ src/Button.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 import './Button.css'; // Tell Webpack that Button.js uses these styles
 
 export default class Button extends Component {
   render() {
     // You can use them as regular CSS styles
-    return <div className="Button">{this.props.name}</div>;
+    return <Link className="Button" to={this.props.linkName}>{this.props.name}</Link>;
   }
 }
